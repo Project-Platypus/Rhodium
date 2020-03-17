514f217381fd30684d2ff79dfda4b683faca5e68
David Hadka <dhadka@users.noreply.github.com>
2020-03-16 22:09:06 -0400

    Update ffi_test.py

--- rhodium/test/ffi_test.py
+++ rhodium/test/ffi_test.py
@@ -46,7 +46,7 @@ class TestNativeModel(unittest.TestCase):
         cls.sopath = os.path.join(rel_dir, libname)
 
     def testNormalReturn(self):
-        os.listdir(".")
+        print(os.listdir("."))
         model = NativeModel("test", "norm_return")
         model.parameters = [Parameter("x", type="double"),
                             Parameter("y", type="double")]
