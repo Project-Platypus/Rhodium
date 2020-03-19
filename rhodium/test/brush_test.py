f7f75ee435796a5f30b344c79ce3b71af7087a70
David Hadka <dhadka@users.noreply.github.com>
2020-03-19 12:23:09 -0400

    Update brush_test.py

--- rhodium/test/brush_test.py
+++ rhodium/test/brush_test.py
@@ -42,7 +42,7 @@ class TestBrush(unittest.TestCase):
         self.assertEquals(assignment[1], brush.name)
         self.assertEquals(assignment[2], RhodiumConfig.default_unassigned_label)
         
-     def testMultipleBrush(self):
+    def testMultipleBrush(self):
         d = {'col1': [1, 2, 3], 'col2': [3, 4, 5]}
         df = pd.DataFrame(data=d)
         brush1 = Brush("col1 < 2")
