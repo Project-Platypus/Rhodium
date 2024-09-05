73d5d250d18b737586d76560f833c79c7ac4a69e
David Hadka <dhadka@users.noreply.github.com>
2024-09-05 14:46:37 +0000

    Use search instead of match

--- rhodium/test/plot_to_savefig.py
+++ rhodium/test/plot_to_savefig.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
 
     with open(sys.argv[1], "r") as f:
         for line in f:
-            m = re.match(r"import matplotlib.pyplot as ([a-zA-Z_]+)", line)
+            m = re.search(r"import matplotlib.pyplot as ([a-zA-Z_]+)", line)
             if m:
                 pyplot_alias = m.group(1)
 
-            m = re.match(pyplot_alias + r".show\(\)", line)
+            m = re.search(pyplot_alias + r".show\(\)", line)
             if m:
                 line = line[:m.start()] + pyplot_alias + ".savefig('" + str(fig_basename) + "." + str(fig_index) + ".png')" + line[m.end():]
                 fig_index += 1
